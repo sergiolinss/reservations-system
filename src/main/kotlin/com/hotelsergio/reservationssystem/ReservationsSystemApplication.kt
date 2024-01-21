9e7c6add97fa32529b9ab63dae2838f66a25d72b
sergiolinss <Sergiovictor2000outlook.com>
2024-01-21 13:08:03 -0300

    inicio do projeto

---
+++ src/main/kotlin/com/hotelsergio/reservationssystem/ReservationsSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.hotelsergio.reservationssystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReservationsSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<ReservationsSystemApplication>(*args)
+}
---
+++ src/test/kotlin/com/hotelsergio/reservationssystem/ReservationsSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hotelsergio.reservationssystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReservationsSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
