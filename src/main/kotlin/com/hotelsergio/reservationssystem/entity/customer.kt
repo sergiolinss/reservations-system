24f41a403fdd90af8c6b27f1c38f38288500aa24
sergiolinss <Sergiovictor2000outlook.com>
2024-01-21 14:29:59 -0300

    criação das entidadess

--- src/main/kotlin/com/hotelsergio/reservationssystem/entity/customer.kt
+++ src/main/kotlin/com/hotelsergio/reservationssystem/entity/customer.kt
@@ -1,4 +1,16 @@
 package com.hotelsergio.reservationssystem.entity
 
-class customer {
-}
+import jakarta.persistence.*
+
+
+@Entity
+data class Customer(
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) @Column(nullable = false) var id: Int? = null,
+    @Column(nullable = false) var nome: String = "",
+    @Column(nullable = true) var sobrenome: String = "",
+    @Column(nullable = false) var email: String = ""
+
+
+
+    )
--- src/main/kotlin/com/hotelsergio/reservationssystem/entity/reservations.kt
+++ src/main/kotlin/com/hotelsergio/reservationssystem/entity/reservations.kt
@@ -1,4 +1,17 @@
 package com.hotelsergio.reservationssystem.entity
 
-class reservations {
-}
+import jakarta.persistence.*
+import java.math.BigDecimal
+import java.time.LocalDate
+import java.util.Date
+import kotlin.random.Random
+
+@Entity
+data class Reservations(
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long? = null,
+    @Column(unique = true, nullable = false) val reservationCode: Long = Random.nextLong(),
+    @Column(nullable = false) val checkInDate: LocalDate,
+    @Column(nullable = false) val checkOutDate: LocalDate,
+    @Column(nullable = false) var reservationValue: BigDecimal = BigDecimal.ZERO
+)
